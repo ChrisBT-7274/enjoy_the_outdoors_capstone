3c1dd71bd0a8925e2e4acef4318b2f5a774ff3cf
Christopher Cerritos <ccerritos2005@gmail.com>
2024-05-24 10:01:50 -0500

    added css files

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -2,19 +2,79 @@
 
 window.onload = () => {
 
-    innitMountainsDropdown();
+    initMountainsDropdown();
 
-    mountainsDropdown = document.querySelector("#mountainSelect")
+    let mountainsDropdown = document.querySelector("#mountainSelect")
 
-    mountainsDropdown.mountainsDropdown("change", displayMountainCard)
+    mountainsDropdown.addEventListener("change", displayMountainCard)
 
 }
 
-function displayMountainCard(event){
+function displayMountainCard(event) {
 
-    let mountainsDiv = document.querySelector("#mountains");
+    let mountainsDiv = document.querySelector("#mountainsArray");
 
-    //clears out the prevous pet
     mountainsDiv.innerHTML = "";
+
+    let mountain = mountainsArray.find((mountain) => mountain.name === event.target.value)
+
+
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "w-25");
+
+    let cardImage = document.createElement("img");
+    cardImage.classList.add("card-img-top", "card-img-fit");
+
+    cardImage.src = mountain.img;
+    cardImage.alt = mountain.name;
+
+    cardDiv.appendChild(cardImage);
+
+    let cardBody = document.createElement("div")
+    cardDiv.classList.add("card-body");
+
+    let cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+
+
+    cardTitle.innerText = mountain.name;
+
+
+    cardBody.appendChild(cardTitle);
+
+
+    let cardText = document.createElement("p");
+    cardTitle.classList.add("card-text");
+
+    cardText.innerHTML = `
+      ${mountain.name} --- ${mountain.desc}. --- ${mountain.elevation} ----- ${mountain.coords}.`
+
+    cardBody.appendChild(cardText);
+
+    cardDiv.appendChild(cardBody)
+
+    mountainsDiv.appendChild(cardDiv)
+}
+
+function initMountainsDropdown() {
+    let mountainsDropdown = document.querySelector("#mountainSelect");
+
+    let defaultOption = document.createElement("option");
+
+    defaultOption.value = "";
+
+    defaultOption.textContent = "Select a Mountain...";
+
+    mountainsDropdown.appendChild(defaultOption);
+
+    mountainsArray.forEach((mountain) => {
+        let newOption = document.createElement("option");
+
+        newOption.value = mountain.name;
+
+        newOption.textContent = mountain.name;
+
+        mountainsDropdown.appendChild(newOption);
+    })
 
 }
